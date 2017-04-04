2760876d91d7200fb2e554c126922024a57bdcb2
aries <arsrymeralles@gmail.com>
2017-04-04 18:57:10 +0800

    update

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -16,53 +16,21 @@
 				alert('Please fill the fields');
 			}
 		}
-
-
-		 
-
-		//this will process dataService compute 
+ 
 		function convert2rpn(){
-			//dataService.convert2rpn(vm.infix);
 			dataService.calculate(vm.infix);
 			vm.rpn = dataService.rpn;
 			dataService.compute();
-
 		}	
 
-		//this will process dataService compute 
-		// function calculate(){
-		// 	dataService.updateFormula(vm.rpn);
-		// 	dataService.compute();
-		// }	
-
-		// add new variables
 		vm.name = '';
 		vm.value = '';
+		vm.rpn = dataService.rpn;
 
-
-		vm.infix = '';
-		vm.formula = '';  // infix expression 5 + ((1 + 2) × 4) − 3
-
-		vm.rpn = dataService.rpn; //  reverse polisth notation 5 1 2 + 4 x + 3 -
-
-		//vm.str = '';
-		vm.test = dataService.test;
-		//vm.expression = dataService.formula;
-		
-		
-		//vm.formula = dataService.formula;
-		
-		
 		// functions
 		vm.saveEntry = saveEntry;
-		
-		 
 		vm.convert2rpn = convert2rpn;
-		//vm.calculate = calculate;
 
-
-		//vm.result = dataService.compute;
-		
 		// services
 		vm.dataService = dataService;
 	}
@@ -72,6 +40,7 @@
 		var vm = this;
 		// services
 		vm.dataService = dataService;
+
 	}
 
 
